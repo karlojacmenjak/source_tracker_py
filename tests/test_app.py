52c3ce4c62e83dd2a0e40ae6b31f0e20a933f120
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-10 14:02:34 +0200

    Add test if we can connect to app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,23 @@
+from typing import Any, Generator
 
+import pytest
+from fastapi.testclient import TestClient
+
+from core.server import app
+
+client: TestClient = TestClient(app=app)
+
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown() -> Generator[None, Any, None]:
+    # Setup
+
+    # Test runner
+    yield
+
+    # Teardown
+
+
+def test_can_connect_to_app():
+    response = client.get("/")
+    assert response.status_code == 200
