26f6efc56151175cd5f44fdc7cc07843077a4508
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-18 17:26:35 +0200

    Add check if content is text/html

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ from typing import Any, Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from httpx import Response
 from pydantic import BaseModel
 from utils import set_test_dir_to_root
 
@@ -22,14 +23,20 @@ def setup_and_teardown() -> Generator[None, Any, None]:
 
 
 class CanConnectTestResults(BaseModel):
-    status_code_ok: bool
+    status_code_ok: bool = True
+    content_is_html: bool = True
 
 
 def test_can_connect_to_app() -> None:
-    expected = CanConnectTestResults(status_code_ok=True)
+    expected = CanConnectTestResults()
 
-    response = client.get("/")
+    response: Response = client.get("/")
 
-    actual = CanConnectTestResults(status_code_ok=response.status_code == 200)
+    is_html = "text/html" in str(response.headers.get("content-type"))
+
+    actual = CanConnectTestResults(
+        status_code_ok=response.status_code == 200,
+        content_is_html=is_html,
+    )
 
     assert actual == expected
