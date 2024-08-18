1d93f00847c45a7e60e720a7fff4314fc51f0fe9
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-18 17:10:23 +0200

    Update test_app to check if webpage is returned

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,8 @@ from typing import Any, Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from pydantic import BaseModel
+from utils import set_test_dir_to_root
 
 from core.server import app
 
@@ -11,6 +13,7 @@ client: TestClient = TestClient(app=app)
 @pytest.fixture(autouse=True)
 def setup_and_teardown() -> Generator[None, Any, None]:
     # Setup
+    set_test_dir_to_root()
 
     # Test runner
     yield
@@ -18,6 +21,15 @@ def setup_and_teardown() -> Generator[None, Any, None]:
     # Teardown
 
 
-def test_can_connect_to_app():
+class CanConnectTestResults(BaseModel):
+    status_code_ok: bool
+
+
+def test_can_connect_to_app() -> None:
+    expected = CanConnectTestResults(status_code_ok=True)
+
     response = client.get("/")
-    assert response.status_code == 404
+
+    actual = CanConnectTestResults(status_code_ok=response.status_code == 200)
+
+    assert actual == expected
---
+++ tests/utils/__init__.py
@@ -0,0 +1,3 @@
+from .test_setup import set_test_dir_to_root
+
+__all__ = ["set_test_dir_to_root"]
