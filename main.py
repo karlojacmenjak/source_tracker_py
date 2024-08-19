e5dbf436433353de8fa2718f676aa6003fa87a35
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-19 15:02:46 +0200

    Add graceful KeyboardInterrupt

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from core.server import run_api
 
 
 async def main() -> None:
-    loop = asyncio.get_event_loop()
     await asyncio.gather(run_bot(), run_api())
 
 
+loop = asyncio.get_event_loop()
+
 if __name__ == "__main__":
     while True:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        try:
+            loop.run_until_complete(main())
+        except KeyboardInterrupt:
+            print("Program exited")
+            loop.close()
