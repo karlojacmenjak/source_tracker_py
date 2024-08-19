b166a03d90c45c46f215b9662521e816c6cf9dec
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-19 20:35:08 +0200

    Init local db in FastAPI lifespan

--- core/server.py
+++ core/server.py
@@ -1,8 +1,19 @@
+from contextlib import asynccontextmanager
+from typing import Any, Generator
+
 from fastapi import FastAPI
 from uvicorn import Server
 
 from api import router
 from core.constant import AppConstants, UvicornConfig
+from core.database.local_database import db
+
+
+@asynccontextmanager
+async def on_startup(app: FastAPI) -> Generator[None, Any, None]:  # type: ignore
+    await db.setup()
+
+    yield
 
 
 def init_routers(app: FastAPI) -> None:
@@ -13,6 +24,7 @@ def create_app() -> FastAPI:
     _app = FastAPI(
         title=AppConstants.title,
         description=AppConstants.description,
+        lifespan=on_startup,
     )
     init_routers(app=_app)
     return _app
