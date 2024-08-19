33d0e7d7f4d7edf48e9bbbe6ddab1416cf92acaf
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-19 16:09:39 +0200

    Add better method of creating asyncio tasks

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from core.server import run_api
 
 
 async def main() -> None:
-    await asyncio.gather(run_bot(), run_api())
+    coros = [run_bot(), run_api()]
+
+    tasks = list(map(asyncio.create_task, coros))
+    try:
+        await asyncio.gather(*tasks, return_exceptions=True)
+    except KeyboardInterrupt:
+        for t in tasks:
+            t.cancel()
+        loop.close()
+        print("Program exited")
+        sys.exit()
 
 
 loop = asyncio.get_event_loop()
 
 if __name__ == "__main__":
-    while True:
-        try:
-            loop.run_until_complete(main())
-        except KeyboardInterrupt:
-            loop.close()
-            print("Program exited")
-            sys.exit()
+    loop.run_until_complete(main())
