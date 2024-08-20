89c47056eb9c05bec7241a58c95431884ef502d6
kjacmenja21 <karlojacmenjak@gmail.com>
2024-08-20 14:08:51 +0200

    Compute guild_image_url from icon

--- app/models/guild.py
+++ app/models/guild.py
@@ -1,6 +1,8 @@
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, computed_field
+
+from core.constant import DiscordAPI
 
 
 class PartialDiscordGuildModel(BaseModel):
@@ -10,3 +12,10 @@ class PartialDiscordGuildModel(BaseModel):
     banner: str | None = None
     owner: bool | None = None
     permissions: int | None = None
+
+    @computed_field
+    @property
+    def guild_image_url(self) -> HttpUrl:
+        if self.icon:
+            return HttpUrl(url=f"/{DiscordAPI.icons_endpoint}/{self.id}/{self.icon}")
+        return HttpUrl(url=DiscordAPI.default_avatar_url)
